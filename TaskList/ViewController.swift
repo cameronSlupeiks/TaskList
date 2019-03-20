f5c723f77bc1112eb7721af63eb8dea5ea75ae10
Cameron Slupeiks <cslupeiks@gmail.com>
2019-03-20 18:53:47 -0400

    Initial Commit

---
+++ TaskList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskList
+//
+//  Created by cam on 2019-03-20.
+//  Copyright © 2019 cam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
